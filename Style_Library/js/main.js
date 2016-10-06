22ad4ec2bb0d071645b56414f4eab3619924ff82
p8queiroz <phqs.phqs@gmail.com>
2016-10-06 09:37:04 -0300

    translating some comments into English.

--- Style_Library/js/main.js
+++ Style_Library/js/main.js
@@ -2,17 +2,23 @@ var protocolo = (String(window.location).indexOf("file:") > -1) ? "http:" : "";
 
 requirejs.config({
 	
-  //base url for modules	
+  /*
+	Base url for modules	
+  */
   baseUrl: 'Style_Library/js',
   
-  //Specify custom paths
+  /*
+	Specify custom paths
+  */
   paths:{ 
 		jQuery: 'libs/jquery/jquery.min',
 		"some": "some/v1.0"
 	},
   
-  /*define the mandatory sequences of dependencies.
-  "oh, sei que é assincrono, mas carregue nesta ordem".*/
+  /*
+    Define the mandatory sequences of dependencies.
+    "I know you are asynchronous, but load with this sequence".
+  */
   shim: {
 		  'source1': ['dependency1','dependency2'],
 		  'source2': ['source1']
